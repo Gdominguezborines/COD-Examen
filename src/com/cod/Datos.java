bd1d246134794d223f288866670a9c67a9875419
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2020-12-19 18:44:07 +0100

    Singletone funcionando  no se porque pero no me permite el cambio de los nombes de los metodos ni de sus returm ya que a visualizar1 le llamaria conexion1 y a con  comprovacionConexion

--- src/com/cod/Datos.java
+++ src/com/cod/Datos.java
@@ -1,23 +1,67 @@
 package com.cod;
 
 public class Datos {
-    private static Datos instance =null;
-    String nombre;
-    String ip = "127.0.0.2";
 
-    Datos() {
-        nombre = "anonymous@danielcastelao.org";
+    String ip = "127.0.0.2";
+    String nombre;
+
+
+    public Datos() {
+
     }
 
-    Datos(String param1) {
+    public void setDatos(String param1) {
         this.nombre = param1;
+
+        // System.out.println(nombre);
+    }
+
+
+    /**
+     * @return metodo que retorna true o false segun
+     */
+
+
+    public boolean con() {
+        //  System.out.println("el nombre que entra es "+nombre+"     222\n");
+
+        if (nombre != "anonymous@danielcastelao.org") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * @param u
+     * @return
+     */
+    public static boolean visualizar1(String usuario) {
+        Datos persona1 = getInstance();
+        // System.out.println(persona1.nombre+"   aqui falla\n");
+
+        System.out.println("Conectando a " + persona1.ip + ", con el usuario " + usuario);
+        return persona1.con();
+    }
+
+    /**
+     * @return
+     */
+    public static boolean Conexion2() {
+        Datos persona2 = Datos.getInstance();
+        System.out.println("Conectando a " + persona2.ip + ", con el usuario " + persona2.nombre);
+        return persona2.con();
     }
 
 
     /**
      * Segun exista o no ya el objeto lo instanciamos o cambiamos los datos
+     *
      * @return la instancia. Si no existe la crea primero
      */
+    private static Datos instance;
+
     public static Datos getInstance() {
         // compruebo si ya está creada
         if (instance == null) {
@@ -30,17 +74,7 @@ public class Datos {
         return instance;
     }
 
-    /**
-     *
-     * @return metodo que retorna true o false segun
-     */
+// cambio new  para hacer llamada a get.instance
 
 
-    public boolean con() {
-        if (nombre != "anonymous@danielcastelao.org") {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -2,51 +2,40 @@ package com.cod;
 
 /**
  * JavaDoc
- *
  */
 public class Main {
     public static void main(String[] args) {
         // creamos un objeto
-        Datos otraInstanciaSeraLaMisma= Datos.getInstance();
         Datos miUnicaInstancia = Datos.getInstance();
 
-/**
- * if que realiza comprovacion segun resultado  emite listo o fallo
- */
 
-        if (visualizar1("anonymous@danielcastelao.org")) {
+        miUnicaInstancia.setDatos("pepe@danielcastelao.org");
+
+
+        //   System.out.println(visualizar1("pepepep "));
+
+        if (Datos.visualizar1("pepe@danielcastelao.org")) {
+            System.out.println("Listo");
+
+            //System.out.println(visualizar1("pepep "));
+        } else {
+            System.out.println("Fallo");
+
+            // System.out.println(visualizar1("pepepep "));
+        }
+
+        Datos otraInstanciaSeraLaMisma = Datos.getInstance();
+
+        otraInstanciaSeraLaMisma.setDatos("anonymous@danielcastelao.org");
+
+
+        if (Datos.Conexion2()) {
             System.out.println("Listo");
         } else {
             System.out.println("Fallo");
         }
 
-        if (visualizar2()) {
-            System.out.println("Listo");
-        } else {
-            System.out.println("Fallo");
-        }
 
     }
-// cambio new  para hacer llamada a get.instance
 
-    /**
-     *
-     * @param u
-     * @return
-     */
-    public static boolean visualizar1(String u) {
-        Datos persona1 =  Datos.getInstance();
-        System.out.println("Conectando a " + persona1.ip + ", con el usuario " + u);
-        return persona1.con();
-    }
-
-    /**
-     *
-     * @return
-     */
-    public static boolean visualizar2() {
-        Datos persona2 =  Datos.getInstance();
-        System.out.println("Conectando a " + persona2.ip + ", con el usuario " + persona2.nombre);
-        return persona2.con();
-    }
 }
